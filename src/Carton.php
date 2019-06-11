4cb8c594e2ea5c4bd6114e12e8959d86c2dc5eef
Luciano Barletta <37297979+lushoBarlett@users.noreply.github.com>
2019-06-11 16:31:36 -0300

    Crea carton segun la clase FabricaCartones

--- src/Carton.php
+++ src/Carton.php
@@ -2,25 +2,25 @@
 
 namespace Bingo;
 
-class CartonEjemplo {
+class Carton {
 
   protected $numeros_carton = [];
 
-  public function __construct($carton) {
-    $numeros_carton = $carton->numeros_carton;
+  public function __construct(array $carton_aleatorio) {
+    $this->numeros_carton = $carton_aleatorio;
   }
 
-  public function filas() {
+  public function columnas() {
     return $this->numeros_carton;
   }
   
-  public function columnas() {
-     $columnas = [];
+  public function filas() {
+     $filas = [];
      $Numeros = $this->numeros_carton;
      for($m=0;$m<9;$m++){
-       $columnas[$m]=array( $Numeros[0] [$m], $Numeros[1] [$m], $Numeros[2] [$m]);
+       $filas[$m]=array( $Numeros[0] [$m], $Numeros[1] [$m], $Numeros[2] [$m]);
      }
-     return $columnas;
+     return $filas;
   }
   
   public function tieneNumero(int $numero) {
