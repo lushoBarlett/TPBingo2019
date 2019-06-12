6f9cf6b19cd698233cda5bd1a2318e198c25c0f4
Luciano Barletta <37297979+lushoBarlett@users.noreply.github.com>
2019-06-11 22:35:43 -0300

    A comma, convenient.

--- tests/VerificacionesAvanzadasCartonTest.php
+++ tests/VerificacionesAvanzadasCartonTest.php
@@ -134,7 +134,7 @@ class VerificacionesAvanzadasCartonTest extends TestCase {
   public static function provider () {
     return [
       [new CartonEjemplo],
-      [new CartonJs]
+      [new CartonJs],
       [new Carton((new FabricaCartones)->generarCarton())]
     ];
   }
